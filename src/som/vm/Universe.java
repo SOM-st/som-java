b8a1806c714ae8f6ede35c59ddd7222119e42c36
Stefan Marr <git@stefan-marr.de>
2025-07-07 17:54:46 +0100

    Universe.print should output on stdout
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/som/vm/Universe.java
+++ src/som/vm/Universe.java
@@ -736,19 +736,19 @@ public class Universe {
 
   public static void print(final String msg) {
     // Checkstyle: stop
-    System.err.print(msg);
+    System.out.print(msg);
     // Checkstyle: resume
   }
 
   public static void println(final String msg) {
     // Checkstyle: stop
-    System.err.println(msg);
+    System.out.println(msg);
     // Checkstyle: resume
   }
 
   public static void println() {
     // Checkstyle: stop
-    System.err.println();
+    System.out.println();
     // Checkstyle: resume
   }
 
